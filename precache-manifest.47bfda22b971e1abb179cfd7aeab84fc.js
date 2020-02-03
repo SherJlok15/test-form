cffa1c21473d671eb6d285c630c68a5468264c68
Fandorin <SherJlok15@gmail.com>
2020-02-03 11:26:08 +0200

    Updates

---
+++ precache-manifest.47bfda22b971e1abb179cfd7aeab84fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac0ead3655620bb3fd232ae46e7fdf0",
+    "url": "/test-form/index.html"
+  },
+  {
+    "revision": "2f406e0237b081a8b43a",
+    "url": "/test-form/static/css/main.cdb9bd33.chunk.css"
+  },
+  {
+    "revision": "cc272172334524c2c5bf",
+    "url": "/test-form/static/js/2.5818e058.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test-form/static/js/2.5818e058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f406e0237b081a8b43a",
+    "url": "/test-form/static/js/main.43cd7b89.chunk.js"
+  },
+  {
+    "revision": "7f96b1766a8742f7151d",
+    "url": "/test-form/static/js/runtime-main.16e6fc16.js"
+  },
+  {
+    "revision": "929a02ee41a697a1274856093198c099",
+    "url": "/test-form/static/media/3-layers.929a02ee.png"
+  },
+  {
+    "revision": "ccad04d46768981ff847f22e8a53b5b3",
+    "url": "/test-form/static/media/HelveticaNeue.ccad04d4.ttf"
+  },
+  {
+    "revision": "b8edca3e45f1f16bc6e20464bd8f2fff",
+    "url": "/test-form/static/media/HelveticaNeueBd.b8edca3e.ttf"
+  },
+  {
+    "revision": "bcd826b1e90968390490c28037857940",
+    "url": "/test-form/static/media/MuseoSansCyrl.bcd826b1.otf"
+  },
+  {
+    "revision": "aebe89187c5ab4e634b3311a94223c67",
+    "url": "/test-form/static/media/return.aebe8918.svg"
+  },
+  {
+    "revision": "c403892b648fc05d9237f8da663a599f",
+    "url": "/test-form/static/media/right_bg.c403892b.png"
+  }
+]);
